5d2d822d780bb3618d2d55e8f0ba3594d8176dfb
RizalAchp <rizal.ahmadp@gmail.com>
2023-04-14 12:20:09 +0700

    Update: Change Logger implementation

--- src/logger.rs
+++ src/logger.rs
@@ -2,35 +2,53 @@
 // https://github.com/rust-lang/rust/issues/44732 stabilises
 
 use log::{Level, LevelFilter, Log, Metadata, Record};
-use std::io;
-use std::io::Write;
-use std::path::Path;
+use std::fs::File;
+use std::io::{self, BufWriter};
+use std::io::{Stderr, Write};
+use std::path::{Path, PathBuf};
 use std::sync::Mutex;
+use std::time::SystemTime;
 
-use crate::DynoResult;
+use crate::{DynoErr, DynoResult};
 
 lazy_static::lazy_static! {
     pub static ref RECORDS_LOGGER: Mutex<Vec<(Level, String)>> = Default::default();
     static ref LOGGER: DynoLogger = DynoLogger::default();
 }
 
-#[derive(Default)]
+trait DynoLoggerInner: Send + 'static {
+    fn log(&mut self, record: &Record);
+    fn flush(&mut self);
+
+    fn roll(&mut self) -> bool {
+        false
+    }
+    fn renew(&mut self) -> DynoResult<'static, ()> {
+        Ok(())
+    }
+}
+
 pub struct DynoLogger {
-    inner: Mutex<Option<DynoLoggerInner>>,
+    inner: Mutex<Box<dyn DynoLoggerInner>>,
+}
+
+impl Default for DynoLogger {
+    fn default() -> Self {
+        Self {
+            inner: Mutex::new(Box::new(ConsoleLogger::new())),
+        }
+    }
 }
 
 impl DynoLogger {
     // Set this `SimpleLogger`'s sink and reset the start time.
     #[allow(unused)]
-    fn renew<T: Write + Send + 'static>(&self, sink: T) -> DynoResult<'_, ()> {
+    fn renew(&self, inner: Box<dyn DynoLoggerInner>) -> DynoResult<'_, ()> {
         let mut lock = self
             .inner
             .lock()
             .map_err(|_| "Failed to lock the mutex Logger")?;
-        *lock = Some(DynoLoggerInner {
-            sink: Box::new(sink),
-        });
-
+        *lock = inner;
         Ok(())
     }
 }
@@ -38,36 +56,133 @@ impl DynoLogger {
 impl Log for DynoLogger {
     #[inline(always)]
     fn enabled(&self, m: &Metadata) -> bool {
-        m.level() < log::max_level()
+        log::log_enabled!(target: "Global", m.level())
     }
 
     #[inline]
     fn log(&self, record: &Record) {
-        if !self.enabled(record.metadata()) {
-            return;
-        }
-        if let Ok(mut locked) = self.inner.lock() {
-            if let Some(ref mut inner) = *locked {
-                inner.log(record);
+        if let Ok(mut inner) = self.inner.lock() {
+            if inner.roll() {
+                match inner.renew() {
+                    Ok(()) => {}
+                    Err(err) => {
+                        eprintln!("ERROR: {err}");
+                        unreachable!()
+                    }
+                }
             }
+            inner.log(record);
+            inner.flush();
         }
     }
 
     #[inline]
     fn flush(&self) {
-        if let Ok(mut locked) = self.inner.lock() {
-            if let Some(ref mut inner) = *locked {
-                inner.flush()
-            }
+        if let Ok(mut inner) = self.inner.lock() {
+            inner.flush()
         }
     }
 }
 
-struct DynoLoggerInner {
-    sink: Box<dyn Write + Send>,
+struct ConsoleLogger {
+    sink: BufWriter<Stderr>,
+}
+impl ConsoleLogger {
+    fn new() -> Self {
+        Self {
+            sink: BufWriter::new(io::stderr()),
+        }
+    }
 }
 
-impl DynoLoggerInner {
+impl DynoLoggerInner for ConsoleLogger {
+    fn log(&mut self, record: &Record) {
+        let level = record.level();
+        writeln!(
+            &mut self.sink,
+            "[{}]:[{}]:{:6} - {}",
+            chrono::Utc::now().format("%+"),
+            record.metadata().target(),
+            level,
+            record.args()
+        )
+        .ok();
+    }
+
+    fn flush(&mut self) {
+        self.sink.flush().ok();
+    }
+}
+
+fn open_file<P: AsRef<Path>>(file: P) -> DynoResult<'static, File> {
+    let file = file.as_ref();
+    std::fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .truncate(false)
+        .create(true)
+        .open(file)
+        .map_err(|err| {
+            DynoErr::filesistem_error(format!(
+                "ERROR: Failed to open file '{}' - {err}",
+                file.display()
+            ))
+        })
+}
+
+#[inline]
+fn format_system_time<'a>(
+    t: &SystemTime,
+    fmt: &'a str,
+) -> chrono::format::DelayedFormat<chrono::format::StrftimeItems<'a>> {
+    let time = t
+        .duration_since(std::time::UNIX_EPOCH)
+        .expect("systemtime since epoch");
+    chrono::NaiveDateTime::from_timestamp_opt(time.as_secs() as i64, time.subsec_nanos())
+        .expect("systemtime to chrono::NaiveDateTime")
+        .format(fmt)
+}
+
+struct FileLogger<W: io::Write + Send + 'static> {
+    #[allow(dead_code)]
+    file: PathBuf,
+    sink: BufWriter<W>,
+    action: RollAction,
+    last_access: SystemTime,
+    max_len: usize,
+    len: usize,
+}
+
+impl FileLogger<File> {
+    fn new(file: PathBuf, action: RollAction, max_len: usize) -> DynoResult<'static, Self> {
+        let fp = open_file(&file)?;
+        let metadata = fp.metadata().map_err(|err| {
+            DynoErr::filesistem_error(format!(
+                "Failed accessing metadata for file {} - {err}",
+                file.display()
+            ))
+        })?;
+
+        let last_access = metadata.modified().map_err(|err| {
+            DynoErr::filesistem_error(format!(
+                "Failed to get time modified info file {} - {err}",
+                file.display()
+            ))
+        })?;
+
+        let len = metadata.len() as usize;
+        Ok(Self {
+            sink: BufWriter::with_capacity(1024, fp),
+            file,
+            action,
+            max_len,
+            len,
+            last_access,
+        })
+    }
+}
+
+impl DynoLoggerInner for FileLogger<File> {
     fn log(&mut self, record: &Record) {
         let level = record.level();
         let record_string = format!(
@@ -77,55 +192,153 @@ impl DynoLoggerInner {
             level,
             record.args()
         );
-        self.sink.write(record_string.as_bytes()).ok();
+        match self.sink.write(record_string.as_bytes()) {
+            Ok(k) => self.len += k,
+            Err(_err) => {}
+        }
 
         if let Ok(mut locked) = RECORDS_LOGGER.lock() {
             locked.push((level, record_string));
         }
     }
+
     #[inline]
     fn flush(&mut self) {
         self.sink.flush().ok();
     }
-}
 
-#[allow(unused)]
-pub fn log_to_file<'a, T: AsRef<Path>>(
-    path: T,
-    max_log_level: LevelFilter,
-    append: bool,
-) -> DynoResult<'a, ()> {
-    let path = path.as_ref();
-    if let Some(parent) = path.parent() {
-        std::fs::create_dir_all(parent)?;
+    fn roll(&mut self) -> bool {
+        if self.len <= self.max_len {
+            return false;
+        }
+        match self.action {
+            RollAction::Noop => false,
+            RollAction::Roll => {
+                let mut file = self.file.clone();
+                file.set_extension(format!(
+                    "log.{}.bak",
+                    format_system_time(&self.last_access, "%+")
+                ));
+                // guaranteed self.file is exists, its ok to ignore error
+                if std::fs::rename(&self.file, file).is_ok() {
+                    self.len = 0;
+                    return true;
+                }
+                false
+            }
+            RollAction::Delete => {
+                let mut ret = true;
+                let renamed = self.file.with_extension("log.bak");
+                if renamed.exists() {
+                    ret = std::fs::remove_file(&renamed).is_err();
+                }
+                if std::fs::rename(&self.file, renamed).is_ok() {
+                    ret = true;
+                }
+                ret
+            }
+        }
     }
-    let file = std::fs::OpenOptions::new()
-        .write(true)
-        .append(append)
-        .truncate(!append)
-        .create(true)
-        .open(path)?;
 
-    LOGGER.renew(file)?;
-    log::set_max_level(max_log_level);
-    // The only possible error is if this has been called before
-    log::set_logger(&*LOGGER).map_err(|err| {
-        crate::DynoErr::new(
-            format!("Failed to set logger - {err}"),
-            crate::ErrKind::AnyError,
-        )
-    })
+    fn renew(&mut self) -> DynoResult<'static, ()> {
+        *self = Self::new(self.file.clone(), self.action, self.max_len)?;
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+pub enum RollAction {
+    #[default]
+    /// do nothing, keep appending log in file
+    Noop,
+    /// Crate new log file and move the old file with prefix `{file}`.`{last-access-time}`.bak
+    Roll,
+    /// delete the old file and create new file with the same name and locations
+    Delete,
+}
+
+const SIZE_TRIGGER_ROLLING_FILE: usize = 10 * 1024 * 1024; // 50Mb
+                                                           //
+#[derive(Debug, Clone)]
+pub struct LoggerBuilder {
+    file: PathBuf,
+    max_level: LevelFilter,
+    roll_action: RollAction,
+    max_size: usize,
+}
+impl Default for LoggerBuilder {
+    fn default() -> Self {
+        Self {
+            file: std::env::temp_dir().join("dynotest/log.log"),
+            max_level: LevelFilter::Warn,
+            roll_action: RollAction::Roll,
+            max_size: SIZE_TRIGGER_ROLLING_FILE,
+        }
+    }
 }
 
 #[allow(unused)]
-pub fn log_to_stderr<'a>(max_log_level: LevelFilter) -> DynoResult<'a, ()> {
-    LOGGER.renew(io::stderr())?;
-    log::set_max_level(max_log_level);
-    // The only possible error is if this has been called before
-    log::set_logger(&*LOGGER).map_err(|err| {
-        crate::DynoErr::new(
-            format!("Failed to set logger - {err}"),
-            crate::ErrKind::AnyError,
-        )
-    })
+impl LoggerBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    pub fn set_file(mut self, file: impl Into<PathBuf>) -> Self {
+        self.file = file.into();
+        self
+    }
+    pub fn set_max_level(mut self, level: impl Into<LevelFilter>) -> Self {
+        self.max_level = level.into();
+        self
+    }
+    pub fn set_roll_action(mut self, action: RollAction) -> Self {
+        self.roll_action = action;
+        self
+    }
+    pub fn set_max_size(mut self, max_in_mb: usize) -> Self {
+        self.max_size = max_in_mb * 1024 * 1024;
+        self
+    }
+
+    pub fn build_console_logger(self) -> DynoResult<'static, ()> {
+        LOGGER.renew(Box::new(ConsoleLogger::new()))?;
+
+        log::set_max_level(self.max_level);
+        log::set_logger(&*LOGGER).map_err(|err| {
+            crate::DynoErr::new(
+                format!("Failed to set logger - {err}"),
+                crate::ErrKind::AnyError,
+            )
+        })
+    }
+
+    pub fn build_file_logger(self) -> DynoResult<'static, ()> {
+        if let Some(parent) = self.file.parent() {
+            std::fs::create_dir_all(parent)?;
+        }
+        let file_logger = FileLogger::new(self.file, self.roll_action, self.max_size)?;
+        LOGGER.renew(Box::new(file_logger));
+        log::set_max_level(self.max_level);
+        log::set_logger(&*LOGGER).map_err(|err| {
+            crate::DynoErr::new(
+                format!("Failed to set logger - {err}"),
+                crate::ErrKind::AnyError,
+            )
+        })
+    }
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     #[test]
+//     fn test_init_file_logger() {
+//         let log_file = PathBuf::from(concat!(
+//             env!("CARGO_MANIFEST_DIR"),
+//             "/tests/files/test_log.log"
+//         ));
+//         match LoggerBuilder::new().set_file(log_file).set_max_level(LevelFilter::Trace).build_file_logger() {
+//             Ok(k) => k,
+//             Err(err) => panic!("ERROR: Failed to build file logger - {err}"),
+//         }
+//     }
+// }
